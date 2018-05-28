53248d3e8dd01a1eaf16638c472643d33dd4856f
Jose Lucena <lucena.jh@gmail.com>
2018-05-27 23:48:03 -0300

    trazer apenas candidatos aprovados para a cipa

--- application/models/CipaDAO.php
+++ application/models/CipaDAO.php
@@ -53,7 +53,7 @@ class CipaDAO extends CI_Model
 
     public function getCandidatosCipa($cipa_id)
     {
-        $resultados = $this->db->query("select usuario.*, candidato.cipa_id from candidato inner join usuario on (candidato.usuario_id = usuario.id) where candidato.cipa_id = ?", [$cipa_id]);
+        $resultados = $this->db->query("select usuario.*, candidato.cipa_id from candidato inner join usuario on (candidato.usuario_id = usuario.id) where candidato.cipa_id = ? and candidato.aprovado = ?", [$cipa_id, 1]);
         $candidatos = [];
         foreach ($resultados->result() as $resultado){
             $candidato = new CandidatoEntity();
